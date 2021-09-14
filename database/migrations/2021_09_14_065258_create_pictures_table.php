0dc623842837276915786bef4f715dd36fdf130f
Peshkova-lab-308 <kristino4ka928@gmail.com>
2021-09-14 10:00:16 +0300

    individual_4 Peshkova 308

---
+++ database/migrations/2021_09_14_065258_create_pictures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('number', 50);
+            $table->string('name', 200);
+            $table->string('author', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
